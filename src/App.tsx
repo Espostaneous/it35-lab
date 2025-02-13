2a43a810dd88af99917aafb822bad771c6b2d855
Espostaneous <ralstone2010@gmail.com>
2025-02-13 10:48:54 +0800

    Fix
    Finally fixed my login and menu, also OCD shifted code lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -35,14 +34,15 @@ import './theme/variables.css';
 import Login from './pages/Login';
 import Menu from './pages/Menu';
 
+
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-      <Route exact path="/it35-lab" component={Login} />
-      <Route exact path="/it35-lab/app" component={Menu} />
+        <Route exact path="/it35-lab" component={Login} />
+        <Route path="/it35-lab/app" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
