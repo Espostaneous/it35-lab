3241a099306f8551df126640655b9a6a0ca007b2
LP, Tejada <20221183@nbsc.edu.ph>
2025-02-27 15:30:19 +0800

    centered dat inputs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -22,13 +22,13 @@ const Login: React.FC = () => {
   }
   return (
     <><>
-      <IonInput label="Email" labelPlacement="floating" fill="outline" placeholder="Enter text"></IonInput>
+      <IonInput label="Email" labelPlacement="floating" fill="outline" placeholder="Enter text" style={{ marginTop: '250px' }}></IonInput>
 
       <br />
 
       <IonInput label="Password" labelPlacement="floating" fill="outline" placeholder="Enter text"></IonInput>
     </><IonContent className='ion-padding'>
-        <IonButton onClick={() => doLogin()} expand="full">
+        <IonButton  onClick={() => doLogin()} expand="full">
           Login
         </IonButton>
       </IonContent></>
